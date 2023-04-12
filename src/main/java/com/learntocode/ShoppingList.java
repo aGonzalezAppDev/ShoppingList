eeddc27b22ec446287f24691a94fec0655693ca4
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-04-12 11:21:35 -0400

    Added a new Java class

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Soap");
+        System.out.println("Detergent");
+        System.out.println("Clothes");
+        System.out.println("Beans");
+        System.out.println("Ice Cream");
+        System.out.println("Candy");
+        System.out.println("Chips");
+        System.out.println("Energy Drinks");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
